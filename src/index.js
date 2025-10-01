48c6adc59f39994e81568981c997603c8d29e79a
smartfilterpro <eric@smartfilterpro.com>
2025-10-01 06:31:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,32 +64,4 @@ process.on('SIGTERM', async () => {
 
 startup();
 
-// src/database/db.js
-const { Pool } = require('pg');
 
-let pool;
-
-function getPool() {
-  if (!pool) {
-    pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-    });
-  }
-  return pool;
-}
-
-async function initDatabase() {
-  const pool = getPool();
-  
-  // Test connection
-  const client = await pool.connect();
-  try {
-    await client.query('SELECT NOW()');
-    console.log('Database connection successful');
-  } finally {
-    client.release();
-  }
-}
-
-module.exports = { getPool, initDatabase };
