63e7378d52764b1c7dc7c4ba7327343f6778c033
smartfilterpro <eric@smartfilterpro.com>
2025-10-01 06:35:35 -0400

    Create pubsubListener.js

---
+++ src/database/pubsubListener.js
@@ -0,0 +1,42 @@
+// src/services/pubsubListener.js
+const { PubSub } = require('@google-cloud/pubsub');
+const { handleDeviceEvent } = require('./runtimeTracker');
+
+let subscription;
+
+async function startPubSubListener() {
+  const pubsub = new PubSub({
+    projectId: process.env.GOOGLE_PROJECT_ID
+  });
+  
+  const subscriptionName = process.env.GOOGLE_PUBSUB_SUBSCRIPTION;
+  subscription = pubsub.subscription(subscriptionName);
+  
+  subscription.on('message', async (message) => {
+    try {
+      const data = JSON.parse(message.data.toString());
+      console.log('Received Pub/Sub message:', JSON.stringify(data, null, 2));
+      
+      await handleDeviceEvent(data);
+      message.ack();
+    } catch (error) {
+      console.error('Error processing Pub/Sub message:', error);
+      message.nack();
+    }
+  });
+  
+  subscription.on('error', (error) => {
+    console.error('Pub/Sub subscription error:', error);
+  });
+  
+  console.log(`Listening for messages on ${subscriptionName}`);
+}
+
+function stopPubSubListener() {
+  if (subscription) {
+    subscription.removeAllListeners();
+    subscription.close();
+  }
+}
+
+module.exports = { startPubSubListener, stopPubSubListener };
